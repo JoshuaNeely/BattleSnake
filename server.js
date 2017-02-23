b81d2820604ed2ea2c56ce4dbd8a6b4ec1a9c265
JoshuaNeely <joshua.a.neely@gmail.com>
2017-02-22 21:05:22 -0600

    Make server listen on port 80

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
 app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(req, res){
+
+// intial file served to a connecting browser
+app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/client.html');
+});
+
+
+http.listen(80, function() {		// 80 is default for web browsers visiting a page
+  console.log('listening on :80');
 });
