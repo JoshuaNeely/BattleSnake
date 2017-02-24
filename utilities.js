656a3790e13ce7425f195c6e919c4d6d10fd145b
JoshuaNeely <joshua.a.neely@gmail.com>
2017-02-23 19:57:51 -0600

    Add 2d matrix generation function

--- utilities.js
+++ utilities.js
@@ -1,13 +1,23 @@
 
-
-
 (function(){
   module.exports = {
-    random : function(min, max) { return local_random(min,max); }
+    random : function(min, max) { return local_random(min,max); },
+    gen_2d_matrix : function(r, c, d) { return local_gen_2d_matrix(r,c,d); }
   }
 }());
 
-
+// ------ local functions to be exported ------
 function local_random(min, max) {
   return Math.floor(Math.random()*(max-min+1))+min;
+}
+
+function local_gen_2d_matrix(rows, columns, initial_data) {
+  var matrix = [];
+  for (var x=0; x<columns; x++) {
+    matrix.push( [] );
+    for (var y=0; y<rows; y++) {
+      matrix[x].push( initial_data );
+    }
+  }
+  return matrix;
 }
