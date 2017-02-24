e17fee40aa1e32b2a26ab9503c7b7c70d96cea56
JoshuaNeely <joshua.a.neely@gmail.com>
2017-02-23 20:37:59 -0600

    Add deep copy function. Fix gen_2d_matrix function

--- utilities.js
+++ utilities.js
@@ -2,7 +2,8 @@
 (function(){
   module.exports = {
     random : function(min, max) { return local_random(min,max); },
-    gen_2d_matrix : function(r, c, d) { return local_gen_2d_matrix(r,c,d); }
+    gen_2d_matrix : function(r, c, d) { return local_gen_2d_matrix(r,c,d); },
+    object_deep_copy: function(old_object) { return local_object_deep_copy(old_object); }
   }
 }());
 
@@ -16,8 +17,16 @@ function local_gen_2d_matrix(rows, columns, initial_data) {
   for (var x=0; x<columns; x++) {
     matrix.push( [] );
     for (var y=0; y<rows; y++) {
-      matrix[x].push( initial_data );
+      matrix[x].push( local_object_deep_copy(initial_data) );
     }
   }
   return matrix;
+}
+
+function local_object_deep_copy(old_object) {
+  var new_object = {};
+  for (key in old_object) {
+    new_object[key] = old_object[key];
+  }
+  return new_object;
 }
