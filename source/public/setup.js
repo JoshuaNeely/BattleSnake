79486c43732805b8c6d3e77cb3b387037c0169d4
JoshuaNeely <joshua.a.neely@gmail.com>
2017-02-27 20:21:15 -0600

    Add default values to setup form

--- source/public/setup.js
+++ source/public/setup.js
@@ -10,13 +10,18 @@ setup.component('setup', {
 
 
 function SetupController($scope, $routeParams, $location) {  
+
   $scope.data = {
+    name : "Guest",
     availableOptions : [
+      {color_name : 'white',  color_value : "#ffffff"},
       {color_name : 'red',    color_value : "#ff0000"},
       {color_name : 'blue',   color_value : "#0000ff"},
       {color_name : 'green',  color_value : "#00ff00"}
     ]
   }
+
+  $scope.data.selectedOption = $scope.data.availableOptions[0];
 
   $scope.submitForm = function() {    
     this.$ctrl.data = {name: $scope.data.name, color: $scope.data.selectedOption.color_value};
